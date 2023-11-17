10e231a2602721a00a86a29366099384de12944d
hajipour <hoseinhajepor@gmail.com>
2023-11-17 16:46:01 +0330

    update core

---
+++ resources/lang/en/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Home' => 'Home',
+    'Category' => 'Category',
+    'Blog' => 'Blog',
+    'LoginRegister' => 'LoginRegister',
+    'Profile' => 'Profile',
+    'Contact' => 'Contact',
+    'GetStarted' => 'GetStarted',
+    'Dashboard' => 'Dashboard',
+    'BuyPlan' => 'Buy Plan',
+    'MyTransactions' => 'My Transactions',
+    'EditProfile' => 'Edit Profile',
+    'logout' => 'logout',
+];
